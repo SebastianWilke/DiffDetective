69e382ce35ea8dc1a20ee40ddcf9427156a87377
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2022-10-13 21:12:03 +0200

    Do not ignore empty lines in tests
    Most of the time, is shouldn't be necessary to ignore empty lines
    because they are collapsed into the same artifact node. This also
    uncovered weird test resources which where not valid diffs (they where
    missing the initial whitespace on empty lines).

--- src/test/java/TestMultiLineMacros.java
+++ src/test/java/TestMultiLineMacros.java
@@ -30,7 +30,7 @@ public class TestMultiLineMacros {
             tree = DiffTreeParser.createDiffTree(
                     fullDiff,
                     true,
-                    true,
+                    false,
                     CPPAnnotationParser.Default);
         }
 
